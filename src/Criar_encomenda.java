d6f4cb8effca751f513ec4516f3e9d6bd32240fb
codinggusta <gusyavo999@gmail.com>
2025-10-04 18:37:18 -0300

    projeto frete

---
+++ src/Criar_encomenda.java
@@ -0,0 +1,26 @@
+import modelo.Encomendas;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Criar_encomenda {
+    public void salvarEncomenda(Encomendas e){
+    String sql = "INSERT INTO objetos (codigo, nome_cliente, telefone, email, documento, descricao, transportadora, status, observacoes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    try(Connection conn = Conexao.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)){
+
+        stmt.setString(1, e.codigo);
+        stmt.setString(2, e.nome_cliente);
+        stmt.setString(3, e.telefone);
+        stmt.setString(4, e.email);
+        stmt.setString(5, e.documento);
+        stmt.setString(6, e.descricao);
+        stmt.setString(7, e.transportadora);
+        stmt.setString(8, e.status);
+
+    }catch (Exception e){
+        e.printStackTrace();
+    }
+    }
+
+}
---
+++ src/modelo/Encomendas.java
@@ -0,0 +1,55 @@
+package modelo;
+
+public class Encomendas {
+    public int id;
+    public String codigo;
+    public String nome_cliente;
+    public String telefone;
+    public String email;
+    public String documento;
+    public String descricao;
+    public String transportadora;
+    public String status;
+    public String observacoes;
+
+    public Encomendas(int id, String codigo, String nome_cliente, String telefone, String email, String documento, String descricao, String transportadora, String status, String observacoes){
+        this.id = id;
+        this.codigo = codigo;
+        this.nome_cliente = nome_cliente;
+        this.telefone = telefone;
+        this.email = email;
+        this.documento = documento;
+        this.descricao = descricao;
+        this.transportadora = transportadora;
+        this.status = status;
+        this.observacoes = observacoes;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public String getNome_cliente(){
+        return nome_cliente;
+    }
+    public String getDocumento(){
+        return documento;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getTransportadora(){
+        return transportadora;
+    }
+    public String getObs(){
+        return observacoes;
+    }
+}
